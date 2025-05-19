d9f8deeb758c8e12e6700bcef2254d87fc9ca7e3
Martin Simonyan <martinsimonyan2563@gmail.com>
2025-05-19 17:05:30 +0400

    app.py

--- EchoLink/app.py
+++ EchoLink/app.py
@@ -4,7 +4,7 @@ import random
 from string import ascii_uppercase
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "nigger"
+app.config["SECRET_KEY"] = "secret_key_123"
 socketio = SocketIO(app)
 
 rooms = {}
